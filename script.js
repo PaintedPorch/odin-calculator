c8868a553d9cbd4eb6da51c23c5e2d47cab8546a
Lars Bakker <Lars.bakker@casema.nl>
2023-07-04 11:39:13 +0200

    Finished main calculator functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const calculation = document.getElementById("calculation");
 const result = document.getElementById("result");
+const zero = document.querySelector(".zero");
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
 const three = document.querySelector(".three");
@@ -10,7 +11,7 @@ const seven = document.querySelector(".seven");
 const eight = document.querySelector(".eight");
 const nine = document.querySelector(".nine");
 const clear = document.querySelector(".clear");
-const random = document.querySelector(".random");
+const ans = document.querySelector(".ans");
 const equals = document.querySelector(".equals");
 const add = document.querySelector(".add");
 const subtract = document.querySelector(".subtract");
@@ -21,69 +22,64 @@ const pi = document.querySelector(".pi");
 const dot = document.querySelector(".dot");
 
 let currentNumStr = "";
+let activeOperatorCount = 0;
+let ansAmount = 0;
+let currentAns = "";
 
+zero.addEventListener("click", function() {
+    calculation.textContent += "0";
+    currentNumStr+="0";
+})
 one.addEventListener("click", function() {
     calculation.textContent += "1";
     currentNumStr+="1";
-    console.log(currentNumStr);
 })
 two.addEventListener("click", function() {
     calculation.textContent += "2";
     currentNumStr+="2";
-    console.log(currentNumStr);
 })
 three.addEventListener("click", function() {
     calculation.textContent += "3";
     currentNumStr+="3";
-    console.log(currentNumStr);
 })
 four.addEventListener("click", function() {
     calculation.textContent += "4";
     currentNumStr+="4";
-    console.log(currentNumStr);
 })
 five.addEventListener("click", function() {
     calculation.textContent += "5";
     currentNumStr+="5";
-    console.log(currentNumStr);
 })
 six.addEventListener("click", function() {
     calculation.textContent += "6";
     currentNumStr+="6";
-    console.log(currentNumStr);
 })
 seven.addEventListener("click", function() {
     calculation.textContent += "7";
     currentNumStr+="7";
-    console.log(currentNumStr);
 })
 eight.addEventListener("click", function() {
     calculation.textContent += "8";
     currentNumStr+="8";
-    console.log(currentNumStr);
 })
 nine.addEventListener("click", function() {
     calculation.textContent += "9";
     currentNumStr+="9";
-    console.log(currentNumStr);
 })
 dot.addEventListener("click", function() {
     calculation.textContent += ".";
     currentNumStr+=".";
-    console.log(currentNumStr);
 })
 
 pi.addEventListener("click", function() {
     calculation.textContent = "3.14159265";
     currentNumStr = "";
     currentNumStr+="3.14159265";
-    console.log(currentNumStr);
 })
 e.addEventListener("click", function() {
     calculation.textContent = "2.71828182";
     currentNumStr = "";
     currentNumStr+="2.71828182";
-    console.log(currentNumStr);
 })
 
 function clearNum()
@@ -91,35 +87,96 @@ function clearNum()
     calculation.textContent = "";
     result.textContent = "";
     currentNumStr = "";
+    activeOperatorCount = 0;
 }
 clear.addEventListener("click", clearNum);
 
-function addNums(a, b)
+function addNums()
 { // Use symbol to create string
     calculation.textContent += "+";
-    return a+b;
+    activeOperatorCount++;
 }
 add.addEventListener("click", addNums);
-function subNums(a, b)
+function subNums()
 { // Use symbol to create string
     calculation.textContent += "-";
-    return a-b;
+    activeOperatorCount++;
 }
 subtract.addEventListener("click", subNums);
-function divNums(a, b)
+function divNums()
 { // Use symbol to create string
     calculation.textContent += "/";
-    return a/b;
+    activeOperatorCount++;
 }
 divide.addEventListener("click", divNums);
-function multNums(a, b)
+function multNums()
 { // Use symbol to create string
     calculation.textContent += "*";
-    return a*b;
+    activeOperatorCount++;
 }
 multiply.addEventListener("click", multNums);
 
+function answer()
+{
+    if (ansAmount == 1)
+    {
+        calculation.textContent += String(currentAns);
+    }
+    else
+    {
+        calculation.textContent = "Ans Error! Ans can only be used once after an operation.";
+    }
+    ansAmount = 0;
+    currentAns = "";
+}
+ans.addEventListener("click", answer);
+
 function equalResult()
 { // split textContent on operator, use switch statement to determine which function to exec
-
+    let a = 0
+    let b = 0;
+    if (activeOperatorCount == 1)
+    {
+        activeOperatorCount = 0;
+        if (calculation.textContent.includes("+")) 
+        {
+            let split_str = calculation.textContent.split("+");
+            a = Number(split_str[0]);
+            b = Number(split_str[1]);
+            currentAns = result.textContent = Math.round((a + b) * 100) / 100;
+            ansAmount++;
+        }
+        else if (calculation.textContent.includes("-")) 
+        {
+            let split_str = calculation.textContent.split("-");
+            a = Number(split_str[0]);
+            b = Number(split_str[1]);
+            currentAns = result.textContent = Math.round((a - b) * 100) / 100;
+            ansAmount++;
+        }
+        else if (calculation.textContent.includes("/")) 
+        {
+            let split_str = calculation.textContent.split("/");
+            a = Number(split_str[0]);
+            b = Number(split_str[1]);
+            currentAns = result.textContent = Math.round((a / b) * 100) / 100;
+            ansAmount++;
+        }
+        else if (calculation.textContent.includes("*")) 
+        {
+            let split_str = calculation.textContent.split("*");
+            a = Number(split_str[0]);
+            b = Number(split_str[1]);
+            currentAns = result.textContent = Math.round((a * b) * 100) / 100;
+            ansAmount++;
+        }
+    }
+    else
+    {
+        calculation.textContent = "Please use a single operator per operation";
+        activeOperatorCount = 0;
+        ansAmount = 0;
+    }
+    activeOperatorCount == 0;
 }
+equals.addEventListener("click", equalResult);
